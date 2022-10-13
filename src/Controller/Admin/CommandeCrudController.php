ef25843ab64023c394bbd2e6ec5f344aa32d4069
Chapasse <h2vregille@gmail.com>
2022-10-13 15:52:31 +0200

    petites modifs

--- src/Controller/Admin/CommandeCrudController.php
+++ src/Controller/Admin/CommandeCrudController.php
@@ -37,7 +37,7 @@ class CommandeCrudController extends AbstractCrudController
             ChoiceField::new('etat')->setChoices(['en cours de traitement'=>'en cours de traitement','envoyé'=>'envoye','livré'=>'livre']),
             AssociationField::new('id_membre')->renderAsNativeWidget(),
             AssociationField::new('id_product')->renderAsNativeWidget(),
-            DateTimeField::new('date_enregistrement')->setFormat(DateTimeField::FORMAT_LONG, DateTimeField::FORMAT_NONE)->hideOnForm(),    
+            DateTimeField::new('date_enregistrement')->renderAsText()->hideOnForm(),    
         ];
     }
     
